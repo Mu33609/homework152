d1d12b949ea9cdd18fe589f260bd1b40bccdd935
Mu33609 <bozorovnozor@gmail.com>
2025-05-12 08:56:49 +0500

    first commit

--- js/main.js
+++ js/main.js
@@ -1,8 +1,55 @@
-const loading = document.getElementById("loading");
-setTimeout(() => (loading.style.display = "none"), 3000);
+window.addEventListener("load", function () {
+  const loaderWrapper = document.getElementById("loader-wrapper");
+  setTimeout(() => {
+    loaderWrapper.style.display = "none";
+  }, 200);
+});
+document.getElementById("dark-mode-toggle").addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+});
 
-let darkBtn = document.querySelector("#dark-btn");
-let body = document.querySelector("body");
-darkBtn.addEventListener("click", () => {
-  body.classList.toggle("dark");
+document.getElementById("hamburger").addEventListener("click", () => {
+  document.querySelector("nav ul").classList.toggle("active");
+});
+const backToTopBtn = document.getElementById("backToTopBtn");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    backToTopBtn.style.display = "block";
+  } else {
+    backToTopBtn.style.display = "none";
+  }
+});
+
+backToTopBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
+let modeBtn = document.querySelector(".site_moon");
+
+modeBtn.addEventListener("click", function () {
+  if (document.body.className != "dark") {
+    this.firstElementChild.src = "./images/sun.svg";
+  } else {
+    this.firstElementChild.src = "./images/moon.svg";
+  }
+  document.body.classList.toggle("dark");
+});
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 5,
+    },
+  },
 });
